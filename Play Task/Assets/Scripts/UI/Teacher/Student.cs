77d03eccbe75bb8a9caf806351b2ad13da4d8fa8
Santhusha Munasinghe <santhushamunasinghe601@gmail.com>
2023-05-08 18:03:56 +0530

    Display Subject Data - Failed

--- Play Task/Assets/Scripts/UI/Teacher/Student.cs
+++ Play Task/Assets/Scripts/UI/Teacher/Student.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -8,7 +9,7 @@ public class Student : TeacherDashboardClassroom
 {
     public StudentData selectedStudent;
     //public List<Dictionary<string, string>> subjectList = new List<Dictionary<string, string>>();
-    public List<ISubjectc> subjectList = new List<ISubjectc>();
+    public List<ISubject> subjectList = new List<ISubject>();
     public List<ITopic> topicList = new List<ITopic>();
     public List<SubtopicData> subtopicList = new List<SubtopicData>();
     public List<ISubtopictResults> subtopicResultsList = new List<ISubtopictResults>();
@@ -20,11 +21,17 @@ public class Student : TeacherDashboardClassroom
     private List<VisualElement> tabList = new List<VisualElement>();
     private List<string> tabTitles = new List<string>();
 
-    //Details Box Visual Elements
+    //Details Box Elements
     private Label nameBox;
     private Label phoneBox;
     private Label emailBox;
     private Label homeBox;
+
+    //Results Box Elements
+    private Label subjectCountLabel;
+    private Label totalLabel;
+    private Label averageLabel;
+    private ScrollView subjectListVew; 
 
     void Start()
     {
@@ -50,7 +57,10 @@ public class Student : TeacherDashboardClassroom
         homeBox = studentBox.Q<VisualElement>("student-detail-home").Q<Label>();
 
         //Results Box
-
+        subjectCountLabel = studentBox.Q<VisualElement>("subjects-count").Q<Label>();
+        totalLabel = studentBox.Q<VisualElement>("total").Q<Label>();
+        averageLabel = studentBox.Q<VisualElement>("avg").Q<Label>();
+        subjectListVew = studentBox.Q<ScrollView>("term-subject-list");
 
         //Buttons
         var nextBtn = studentBox.Q<Button>("student-next-btn");
@@ -130,7 +140,7 @@ public class Student : TeacherDashboardClassroom
             sendRequests.SendGetRequest(GlobalData.url + "/getgradebyid/" + responseJson["grade"], headers, label, (responseJson) => {
                 gradeResponse = responseJson["number"].Value<string>();
 
-                ISubjectc newSubject = new ISubjectc();
+                ISubject newSubject = new ISubject();
                 newSubject.SubjectID = idResponse;
                 newSubject.Name = nameResponse;
                 newSubject.Grade = gradeResponse;
@@ -164,6 +174,22 @@ public class Student : TeacherDashboardClassroom
                                         newSubtopic.Title = topicObject["title"].Value<string>();
 
                                         subtopicList.Add(newSubtopic);
+
+                                        sendRequests.SendGetRequest(GlobalData.url + $"/getattempts/{selectedStudent.StdID}/{newSubtopic.SbtID}", headers, label, (responseJson) =>
+                                        {
+                                            ISubtopictResults newResult = new ISubtopictResults();
+                                            newResult.ResultsID = responseJson["_id"].Value<string>();
+                                            newResult.SubtopicID = responseJson["subtopic"].Value<string>();
+
+                                            newResult.GameplayData = new GameplayData();
+                                            newResult.GameplayData.StartDateTime = responseJson["starttime"].Value<string>();
+                                            newResult.GameplayData.EndDateTime = responseJson["endtime"].Value<string>();
+                                            newResult.GameplayData.Duration = responseJson["duration"].Value<float>();
+                                            newResult.GameplayData.FinalScore = responseJson["duration"].Value<float>();
+
+                                            string levelScoreString = responseJson["levelscore"].Value<string>();
+                                            newResult.GameplayData.GameLevelData = JsonConvert.DeserializeObject<List<GameplayLevelData>>(levelScoreString);
+                                        });
                                     }
                                 }
                             });
@@ -172,6 +198,50 @@ public class Student : TeacherDashboardClassroom
                 });
             });
         });
+    }
+
+    private void PopulateSubjectResults(string subjectName, float total = 0, float average = 0)
+    {
+        //CREATE
+        VisualElement newSubjectElement = new VisualElement();
+
+        VisualElement subjectNameElement = new VisualElement();
+        Label subjectLabel = new Label();
+
+        VisualElement totalElement = new VisualElement();
+        Label totalLabel = new Label();
+
+        VisualElement avgElement = new VisualElement();
+        Label avgLabel = new Label();
+
+        //ADD CLASSES
+        newSubjectElement.AddToClassList("list-primary-item");
+
+        subjectNameElement.AddToClassList("list-primary-item-text");
+        subjectLabel.AddToClassList("student-item-label");
+
+        totalElement.AddToClassList("list-primary-item-text");
+        totalLabel.AddToClassList("student-item-label");
+
+        avgElement.AddToClassList("list-primary-item-text");
+        avgLabel.AddToClassList("student-item-label");
+
+        //ADD VALUES
+        subjectLabel.text = subjectName;
+        totalLabel.text = total.ToString();
+        avgLabel.text = average.ToString();
+
+        //ADD ELEMENTS
+
+        //Add Labels
+        subjectNameElement.Add(subjectLabel);
+        totalElement.Add(totalLabel);
+        avgElement.Add(avgLabel);
+
+        //Add to newSubjectElement
+        newSubjectElement.Add(subjectNameElement);
+        newSubjectElement.Add(totalElement);
+        newSubjectElement.Add(avgElement);
     }
 }
 
@@ -183,7 +253,7 @@ public interface ISubjectContainer
     string Grade { get; set; }
 }
 
-public class ISubjectc : ISubjectContainer
+public class ISubject : ISubjectContainer
 {
     public string SubjectID { get; set; }
     public string Name { get; set; }
@@ -211,7 +281,6 @@ public interface ISubtopicResultsContainer
 {
     string ResultsID { get; set; }
     string SubtopicID { get; set; }
-    string SubtopicName { get; set; }
     GameplayData GameplayData { get; set; }
 }
 
@@ -219,6 +288,5 @@ public class ISubtopictResults : ISubtopicResultsContainer
 {
     public string ResultsID { get; set; }
     public string SubtopicID { get; set; }
-    public string SubtopicName { get; set; }
     public GameplayData GameplayData { get; set; }
 }
